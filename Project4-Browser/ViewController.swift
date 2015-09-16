aa6c93d54b52ae3d4dbb64ea5d7ce6b64ea3d86a
Robert McBryde <robert.mcbryde@kotikan.com>
2015-09-16 23:23:46 +0100

    Initial Commit

---
+++ Project4-Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project4-Browser
+//
+//  Created by Robert McBryde on 16/09/2015.
+//  Copyright © 2015 Robert McBryde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
