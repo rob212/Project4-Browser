054dcdb51586d37af3f5d8b09678bb306793cdc5
Robert McBryde <robert.mcbryde@kotikan.com>
2015-09-20 16:38:53 +0100

    EOC - Chapter 4 Webview
    Completed project. WebView is loaded as default view directing to hackingWithSwift website. Page can be refreshed and there is a UIProgressBar in the UIToolbar also. All UIView have been manipulated via code without the use of StoryBoards in Interface builder.
    
    There is a UIBarButton added to ope nan ActionSheet with the allowed sites the user can visit. KVO (Key Value Observer) pattern has been used in order to link the UIProgressBar to the estimatedProgress property on the webView. Further improvements to add in the future will include an initial TableView containing a list of the URLs you can visit. Also an UIAlertView explaining why a link cannot be clicked as it's not in the allowed list of sites.

--- Project4-Browser/ViewController.swift
+++ Project4-Browser/ViewController.swift
@@ -12,6 +12,8 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView : WKWebView!
+    var progressView : UIProgressView!
+    var websites = ["apple.com", "microsoft.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -22,16 +24,75 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "https://www.hackingwithswift.com")!
-        webView.loadRequest(NSURLRequest(URL: url))
+        openPage(websites[2])
         webView.allowsBackForwardNavigationGestures = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: UIBarButtonItemStyle.Plain, target: self, action: "openTapped")
+        
+        progressView = UIProgressView(progressViewStyle: .Default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .Refresh, target: webView, action: "reload")
+        
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: NSKeyValueObservingOptions.New, context: nil)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.toolbarHidden = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .ActionSheet)
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: UIAlertActionStyle.Default, handler: openPageFromAction))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func openPageFromAction(action: UIAlertAction!) {
+        openPage(action.title!)
+    }
+    
+    func openPage(page: String) {
+        if let url = NSURL(string: "https://" + page) {
+            webView.loadRequest(NSURLRequest(URL: url))
+        }
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if keyPath! == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.URL {
+            
+            if let host = url.host {
+                for website in websites {
+                    if host.rangeOfString(website) != nil {
+                        decisionHandler(.Allow)
+                        return
+                    }
+                }
+            }
+        }
+        decisionHandler(.Cancel)
+    }
+    
+    deinit {
+        webView.removeObserver(self, forKeyPath: "estimatedProgress")
+    }
+    
 }
 
